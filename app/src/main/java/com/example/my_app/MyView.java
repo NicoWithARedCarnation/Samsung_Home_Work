c359c307914c85dc7aa97969d700b03e811fd131
Ania Bogatova <ania.daa11@gmail.com>
2019-10-16 12:00:36 +0300

    1.8.11 анимация-шарики+линии

--- app/src/main/java/com/example/my_app/MyView.java
+++ app/src/main/java/com/example/my_app/MyView.java
@@ -1,6 +1,8 @@
 package com.example.my_app;
+
 import android.content.Context;
 import android.graphics.Canvas;
+import android.graphics.Color;
 import android.graphics.Paint;
 import android.view.View;
 public class MyView extends View {
@@ -23,17 +25,21 @@ public class MyView extends View {
     protected void onDraw(Canvas canvas){
         // отрисовываем все шарики
         for (int i = 0; i < N; i++) {
-            canvas.drawCircle(x[i], y[i], 15, paint);
+            canvas.drawCircle(x[i], y[i], 10, paint);
         }
+        for (int i = 0; i < N - 1; i++) { //добавила линии
+            canvas.drawLine(x[i], y[i], x[i + 1], y[i + 1], paint);
+        }
+        paint.setColor(Color.BLUE); //поменла цвет
         // готовим массивы x и у для следущего кадра
         for (int i = 0; i < N; i++) {
             x[i] += vx[i];
             y[i] += vy[i];
             if (x[i] < 15 || x[i] > this.getWidth()-10){  // шарики не выходят за поля экрана
-                x[i] = (float) (Math.random() * 500);
+                vx[i] = - vx[i];
             }
             if (y[i] < 15 || y[i] > this.getHeight()-10){
-                y[i] = (float) (Math.random() * 500);
+                vy[i] = - vy[i];
             }
         }
         //запрашиваем перерисовку
