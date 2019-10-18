45e2b93690b248b19b22075b6f860d06d20a4a9c
Ania Bogatova <ania.daa11@gmail.com>
2019-10-18 18:49:22 +0300

    1.9.5 анимация-функции

--- app/src/main/java/com/example/my_app/MyView.java
+++ app/src/main/java/com/example/my_app/MyView.java
@@ -5,42 +5,57 @@ import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.view.View;
+
+
 public class MyView extends View {
+
+
     int N = 50; // количество шариков
     float[] x = new float[N];
     float[] y = new float[N];
     float[] vx = new float[N];
     float[] vy = new float[N];
-    public MyView(Context context) {
-        super(context);
-        for (int i = 0; i < N; i++) {
-            x[i] = (float) (Math.random() * 500);
-            y[i] = (float) (Math.random() * 500);
-            vx[i] = (float) (Math.random() * 6 - 3);
-            vy[i] = (float) (Math.random() * 6 - 3);
+
+    float rand(float min , float max){
+        return (float)(Math.random() * (max - min + 1)) + min;
+    }
+    void fillRandom(float[] array , float min, float max){
+        for (int i = 0; i < array.length; i++){
+            array[i] = rand (min, max);
         }
     }
+    public MyView(Context context) {
+        super(context);
+        fillRandom(x, 0, 500);
+        fillRandom(y, 0, 500);
+        fillRandom(vx, -3, 3);
+        fillRandom(vy, -3,3);
+    }
     Paint paint = new Paint();
+    void add(float[] array , float[] values){
+        for (int i = 0; i < array.length; i++){
+            array[i] += values[i];
+        }
+    }
     @Override
     protected void onDraw(Canvas canvas){
         // отрисовываем все шарики
         for (int i = 0; i < N; i++) {
             canvas.drawCircle(x[i], y[i], 10, paint);
-        }
-        for (int i = 0; i < N - 1; i++) { //добавила линии
-            canvas.drawLine(x[i], y[i], x[i + 1], y[i + 1], paint);
+
         }
         paint.setColor(Color.BLUE); //поменла цвет
         // готовим массивы x и у для следущего кадра
         for (int i = 0; i < N; i++) {
-            x[i] += vx[i];
-            y[i] += vy[i];
+            add(x, vx);
+            add(y, vy);
             if (x[i] < 15 || x[i] > this.getWidth()-10){  // шарики не выходят за поля экрана
                 vx[i] = - vx[i];
             }
             if (y[i] < 15 || y[i] > this.getHeight()-10){
                 vy[i] = - vy[i];
             }
+
         }
         //запрашиваем перерисовку
         invalidate();
